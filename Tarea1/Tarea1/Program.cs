ad157c57d6c3ee8be0c3c4a17531e17de292b513
RobertoVallejo <vallejoroberto19@gmail.com>
2024-12-28 21:52:11 -0500

    Tarea 1

---
+++ Tarea1/Tarea1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Roberto Vallejo");
+            Console.WriteLine("Mexico");
+            Console.WriteLine("Mi estatura es 1.75m");
+        }
+    }
+}
